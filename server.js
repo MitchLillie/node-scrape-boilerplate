b8668896d4a9642e5f016650a91f475d40463361
Mitch Lillie <mitch@mitchlillie.com>
2016-10-08 10:25:52 -0700

    use cheerio to load data and actually return something

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cheerio = require('cheerio')
 const express = require('express')
 const request = require('request')
 const app = express()
@@ -5,9 +6,27 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
-  var url = 'http://www.upi.com/Odd_News/'
-  request(url, function (err, res, body) {
-    console.log("body: ", body)
+  // res.setHeader('Content-Type', 'text/html')
+
+  let url = 'http://www.upi.com/Odd_News/'
+  let promise = new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (err || !body) {
+        reject(err)
+      }
+      let headlines = []
+      let $ = cheerio.load(body)
+      $('.upi_item').each(function (i, e) {
+        let $this = $(this)
+        headlines.push($('span.hl', $this).text())
+      })
+      resolve(headlines)
+    })
+  })
+  promise.then(function (data) {
+    res.send('<h3>' + data.join('</h3><h3>') + '</h3>')
+  }).catch(function (err) {
+    throw new Error(err)
   })
 })
 
